e7720ed2a52d1ca8fca72ebe7f2a36f1459a6e0b
Ram11Coder <rambeliver11@gmail.com>
2022-01-03 23:36:22 +0530

    Modified

--- HackerRank/arrays/Array2DHourGlass.java
+++ HackerRank/arrays/Array2DHourGlass.java
@@ -25,14 +25,12 @@ class Result2 {
 		int sum = 0, max = Integer.MIN_VALUE;
 		for (int i = 0; i < arr.size() - 2; i++) {
 			for (int j = 0; j < arr.get(i).size() - 2; j++) {
-
 				sum = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2) + arr.get(i + 1).get(j + 1)
 						+ arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
 				if (max < sum)
 					max = sum;
 			}
 		}
-		System.out.println(max);
 		return max;
 
 	}
